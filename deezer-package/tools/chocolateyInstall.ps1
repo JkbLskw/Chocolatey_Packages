44893fc48bd8db056587a2b3ad4616cb7e444880
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2020-03-10 20:57:00 +0100

    update deezer 4.18.50/4.18.60

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '7f5fc641c35637aac7481fd8bd3feea3db630fa3a4606db8faefba6467417c18'
+  checksum      = '7d31da5d5f38383a2ccd84f8027b906a8b64f83745ed5b0d22d4b61f744956ef'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
