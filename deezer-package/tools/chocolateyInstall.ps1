6d38cf2f9150072f5b4eeb3880acc99b213b976b
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2020-04-12 00:31:40 +0200

    Update Deezer (4.19.0), Elsterformular (21.1.0)

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '7d31da5d5f38383a2ccd84f8027b906a8b64f83745ed5b0d22d4b61f744956ef'
+  checksum      = '676c85bb77f7c28962a4ac59baf4ed4e564475c559fc1df44ce678826075e756'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- elsterformular-package/tools/chocolateyInstall.ps1
+++ elsterformular-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'msi'
   url           = 'https://download.elster.de/aktuell/ElsterFormularKomplett.msi'
   silentArgs 	= '/quiet'
-  checksum      = '4D57E292E6659643D79EAC98D236EE0998FD063E1C264071BF7A8DDA58EEF765'
+  checksum      = '4d57e292e6659643d79eac98d236ee0998fd063e1c264071bf7a8dda58eef765'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
