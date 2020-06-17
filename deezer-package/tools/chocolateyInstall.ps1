3271f2c69476469121ff54b514d15eb76d265aa6
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2020-06-17 13:56:37 +0200

    Deezer Update; Update Deezer-Copyright

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = 'c17d343fad67ba1f093e4477cb8d899c0e33b4717be540f878db0fb93811747a'
+  checksum      = 'b0d5069c2c4b34d1b6fb37a345692da7164fe97cc63d95a08561b1cfecc34ce8'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
