67789324e9ad896dc50a8b4b6ab4f0d0b199c34c
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2020-11-02 17:04:14 +0100

    cocuun update 2.11.2

--- cocuun-package/tools/chocolateyInstall.ps1
+++ cocuun-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.cocuun.de/desktop/Cocuun-Setup.exe'
   silentArgs 	= '/S ALLUSERS=1'
-  checksum      = 'dc1c9bfb72a2febcfbbb21abbf01f8d2262e4dec3e84e0687a3088431673d96e'
+  checksum      = 'a6fc4b5989d2281aaa38d73d9a59b47e1c8ccb27ea063a4db5153f6d1513fff9'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
