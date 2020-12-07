7da3dbe09aac90a70d99f472052367484e3fce25
jansohn <jansohn@users.noreply.github.com>
2020-12-08 00:25:09 +0100

    Fixes Jameica 2.10.0 package (#2)
    * fixed download URLs and checksums for version 2.10.0
    
    * rebuilt package for version 2.10.0
    
    Co-authored-by: jansohn <jansohn@yopmail.com>

--- jameica-package/tools/chocolateyinstall.ps1
+++ jameica-package/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win32-2.8.6.zip' # download url, HTTPS preferred
-$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.8.6.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win32-2.10.0.zip' # download url, HTTPS preferred
+$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.10.0.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,10 +10,10 @@ $packageArgs = @{
   
   softwareName  = 'jameica*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '7A0A2815E0C4BBB2443D1AC8C29240192ADA01C3'
-  checksumType  = 'sha1'
-  checksum64    = '1E4F27642F60F76ABDE2943A22E843A21976A6AA'
-  checksumType64= 'sha1'
+  checksum      = '6c9994d5a6eb9de7d60e15b351fb13635fbaee1badf8843c62323d6a81732d9a'
+  checksumType  = 'sha256'
+  checksum64    = '403e61a55decfc3fe1f30b31e24d0ca709480597ef67c49c1a80e7c2ba03ed82'
+  checksumType64= 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
