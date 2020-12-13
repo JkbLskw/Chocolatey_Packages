9ae61adf9678a366adbe3e2a6f249afd199a0f01
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2020-12-13 14:40:25 +0100

    Deezer Update 4.29.0

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '514108d328a7c995588b11ebbd0384cc54dc0b3ebedbcf6a8d444467e80cb2c2'
+  checksum      = 'e9246ed328bbc78bd55ff37114518f6dd81889c392c3fcb8b48665cddcc50c03'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
