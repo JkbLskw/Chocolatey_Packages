19f25462b442d55779b42a3c786ae9720bfb38bf
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2021-06-08 22:08:20 +0200

    Deezer Update 5.20.0

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '2830062f908c25750b39bfc4880bb16088a23ea95fd4743b54a2c25df8a2d548'
+  checksum      = '11bd5a7f2cd58b5ec7e879f405477440669add5e6cf207050ce6168cb8690fa6'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
