b3e4af4446a151bfeafdd39c4b4a392e9432663f
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2021-08-14 12:00:07 +0200

    Deezer Update 5.30.30

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '9741cd8538658a9dccd76167d51b4f93bb8d2b4b5e00f6f608a8b9bb3e17ddc8'
+  checksum      = '582b3df3207f8cd20f92af5917e65f75eab73cbc370244600c6ef872e7837e92'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- jameica-package/tools/chocolateyinstall.ps1
+++ jameica-package/tools/chocolateyinstall.ps1
@@ -1,14 +1,13 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win32-2.10.0.zip' # download url, HTTPS preferred
-$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.10.0.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://www.artsoft.org/RELEASES/win32/rocksndiamonds/rocksndiamonds-4.2.3.1-win32-setup.exe' # download url, HTTPS preferred
+$url64      = 'https://www.artsoft.org/RELEASES/win64/rocksndiamonds/rocksndiamonds-4.2.3.1-win64-setup.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   url64bit      = $url64
-  
-  softwareName  = 'jameica*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'rocksndiamonds*'
 
   checksum      = '403e61a55decfc3fe1f30b31e24d0ca709480597ef67c49c1a80e7c2ba03ed82'
   checksumType  = 'sha256'
