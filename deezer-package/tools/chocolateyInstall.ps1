c4f524b383a5a2278285b68fd79e51d3e6147141
Jakob Liskow <jakob.liskow@vivaldi.net>
2021-12-04 10:37:56 +0100

    Update Deezer to 5.30.110

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download?platform=win32&architecture=x86'
   silentArgs 	= '/quiet'
-  checksum      = '7be6dcb99c3898b287d54c775365433ea2d0d6bfd0c0a39ac647e72730cd3575'
+  checksum      = 'ec874ed8d9ee2377a8cafda6d021d735bc3c1d1de87445b776561c855213a641'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
