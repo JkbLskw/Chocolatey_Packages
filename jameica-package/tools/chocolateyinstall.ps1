200493a30de1a5c4ed0d54e49d8d680501615c6a
Jakob Liskow <jakob.liskow@vivaldi.net>
2021-12-04 10:56:56 +0100

    Update Jameica to 2.10.1

--- jameica-package/tools/chocolateyinstall.ps1
+++ jameica-package/tools/chocolateyinstall.ps1
@@ -1,17 +1,18 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.artsoft.org/RELEASES/win32/rocksndiamonds/rocksndiamonds-4.2.3.1-win32-setup.exe' # download url, HTTPS preferred
-$url64      = 'https://www.artsoft.org/RELEASES/win64/rocksndiamonds/rocksndiamonds-4.2.3.1-win64-setup.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win32-2.10.1.zip' # download url, HTTPS preferred
+$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.10.1.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   url64bit      = $url64
-  softwareName  = 'rocksndiamonds*'
+  
+  softwareName  = 'jameica*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '403e61a55decfc3fe1f30b31e24d0ca709480597ef67c49c1a80e7c2ba03ed82'
+  checksum      = '27EA1856DBBC7BB6E8FD63E4F2FFC5CDF75A5DFFEB98A6A0A07971F53F47CC85'
   checksumType  = 'sha256'
-  checksum64    = '403e61a55decfc3fe1f30b31e24d0ca709480597ef67c49c1a80e7c2ba03ed82'
+  checksum64    = '1050EF269B3B4501075E2FD8D7C4B460499EF0AE9AE33A04E38C92756936AE82'
   checksumType64= 'sha256'
 }
 
