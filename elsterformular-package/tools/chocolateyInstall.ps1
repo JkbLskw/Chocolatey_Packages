07e3f717f3d09bd803bb1ceb2d7405950a0a111e
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2023-09-16 15:10:38 +0200

    Update ElsterFormular to 21.6.0

--- elsterformular-package/tools/chocolateyInstall.ps1
+++ elsterformular-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'msi'
   url           = 'https://download.elster.de/aktuell/ElsterFormularKomplett.msi'
   silentArgs 	= '/quiet'
-  checksum      = 'f7873ae654c04d1929e8114685257ebd5cd85aba7389343757b1eaaf898759dd'
+  checksum      = 'e5dbd88ddf88f1533bde0c701fb41bce6a2b1de54b2eb43068b358b469845677'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
