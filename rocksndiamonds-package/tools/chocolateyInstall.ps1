7c5cb2714e05fca5a41067ed9097d40c1254b091
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2023-09-16 15:02:04 +0200

    Update Rocks'n'Diamonds to 4.3.6.0

--- rocksndiamonds-package/tools/chocolateyInstall.ps1
+++ rocksndiamonds-package/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageArgs = @{
   packageName   = "Rocks'n'Diamonds"
   fileType      = 'exe'
-  fileFullPath   = "$(Get-PackageCacheLocation)\rocksndiamonds-4.2.3.1-win64-setup.exe"
-  url           = 'https://www.artsoft.org/RELEASES/win64/rocksndiamonds/rocksndiamonds-4.2.3.1-win64-setup.exe'
+  fileFullPath   = "$(Get-PackageCacheLocation)\rocksndiamonds-4.3.6.0-win64-setup.exe"
+  url           = 'https://www.artsoft.org/RELEASES/windows/rocksndiamonds/rocksndiamonds-4.3.6.0-win64-setup.exe'
   silentArgs 	= '/S'
-  checksum      = '85559E01C93131FEA18308E3F1BB327252B11AB859F3168A0DB7E9F82500732E'
+  checksum      = '390DAA5A0AEF0955B7C4E3F1E226CDECD72EFB806831D498C0443C5211F24DF2'
   checksumType  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
