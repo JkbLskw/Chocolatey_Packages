2ca8ec1aa1f17d110af79d537d76d195e0026618
JkbLskw <5z4hsex7Yc3f4XOkXRrP>
2023-09-30 17:16:59 +0200

    Update Deezer 5.30.660.187

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageArgs = @{
   packageName   = 'Deezer-Desktop'
   fileType      = 'exe'
-  url           = 'https://www.deezer.com/desktop/download/artifact/win32/x86/5.30.650'
+  url           = 'https://www.deezer.com/desktop/download/artifact/win32/x86/5.30.660'
   silentArgs    = '/quiet'
-  checksum      = '68ffbc0e4fc55a1ecd04d970bc10a8b96bd11f7e5b261b5f269d57c15666ffad'
+  checksum      = '708ca6a4a34f897ddf5c322717050930ee6da9ad008a74665d8d7250c3bd02bc'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
