acde5e09daf60cace1c88af0d0837e473124ca69
JkbLskw <gj2sprpwcv@hsrheinmain.onmicrosoft.com>
2025-11-21 10:12:00 +0100

    update deezer and jameica

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageArgs = @{
   packageName   = 'Deezer-Desktop'
   fileType      = 'exe'
-  url           = 'https://www.deezer.com/desktop/download/artifact/win32/x86/5.30.660'
+  url           = 'https://www.deezer.com/desktop/download/artifact-win32-x86-7.1.0'
   silentArgs    = '/quiet'
-  checksum      = '708ca6a4a34f897ddf5c322717050930ee6da9ad008a74665d8d7250c3bd02bc'
+  checksum      = '9c34b7c92b709886b21a9e1dc6797d0b53dd84709aa8e6197c271b7e9cae133c'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- jameica-package/tools/chocolateyinstall.ps1
+++ jameica-package/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win32-2.10.4.zip' # download url, HTTPS preferred
-$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.10.4.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-2.12.0.zip' # download url, HTTPS preferred
+$url64      = 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-win64-2.12.0.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{	
   packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,9 @@ $packageArgs = @{
   
   softwareName  = 'jameica*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = 'a8cf0fa00f23bf1b4c5d2ac5d17c1deda1ad269bb25637ad512151a61e385ffd'
+  checksum      = '7a9ec5985563532976e24a3aba97f4f96cef168eae52edd8a85937f8c53fa301'
   checksumType  = 'sha256'
-  checksum64    = 'a8cf0fa00f23bf1b4c5d2ac5d17c1deda1ad269bb25637ad512151a61e385ffd'
+  checksum64    = 'e1df0beedf47007b550c0e42686c7da2e7aebd8f747d49b4bb5adb60ad451108'
   checksumType64= 'sha256'
 }
 
