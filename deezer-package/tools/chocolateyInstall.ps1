17475602d46f3d68cc7c9702389b30001eda4aef
JkbLskw <gj2sprpwcv@hsrheinmain.onmicrosoft.com>
2026-02-08 12:02:55 +0100

    update deezer 7.1

--- deezer-package/tools/chocolateyInstall.ps1
+++ deezer-package/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = 'https://www.deezer.com/desktop/download/artifact-win32-x86-7.1.0'
   silentArgs    = '/quiet'
-  checksum      = '9c34b7c92b709886b21a9e1dc6797d0b53dd84709aa8e6197c271b7e9cae133c'
+  checksum      = '9C34B7C92B709886B21A9E1DC6797D0B53DD84709AA8E6197C271B7E9CAE133C'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
